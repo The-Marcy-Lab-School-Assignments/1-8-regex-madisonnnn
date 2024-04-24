193744383713ef723578f2dad5f81c838705e631
Itzel01 <itzel.areymi.rendon@gmail.com>
2024-04-24 14:19:43 -0400

    feedback

--- src/modify.js
+++ src/modify.js
@@ -1,3 +1,4 @@
+/** FEEDBACK: Here you can use .replace to update the characters and use test to check if they are lowercase or uppercase! */
 const swapAllCases = (str) => {
   const regex = /[a-zA-Z]/g
   return str.replace(regex, )
